e0004047fa40327d058973a72c54e105b895627f
HAKHEO <pigpannyaung@gmail.com>
2026-06-29 14:58:19 +0630

    First Server Upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ try {
 const app = express()
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send("Hello Bagan-360");
+    res.send("Hello Bagan APP");
 });
 
 // Express Packages

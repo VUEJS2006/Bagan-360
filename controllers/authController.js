4458460f67670473802c63319e2f917269fc1e00
HAKHEO <pigpannyaung@gmail.com>
2026-06-30 17:33:45 +0630

    Verify OTP

--- controllers/authController.js
+++ controllers/authController.js
@@ -115,7 +115,7 @@ export const verifyOTP = asyncHandel(async (req, res) => {
 
         );
         res.status(201).json({
-            message: "Register success, waiting admin approval",
+            message: "Register Success",
             success: true,
         });
 

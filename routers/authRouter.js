2960986001fb09340c27e49b2271a1ae2d304165
HAKHEO <pigpannyaung@gmail.com>
2026-07-06 12:57:21 +0630

    Profile

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,4 @@
 import { AccountDelete, register, verifyOTP, login, userList, roleUpdate, logout, userProfile, userProfileEdit, userChangePassword } from "../controllers/authController.js";
-import { upload } from "../middlewares/upload.js";
 import { validateRegister } from "../middlewares/authMiddleware.js";
 import { authenticated, isAdmin } from "../middlewares/authenticatedMiddleware.js";
 import express from "express";

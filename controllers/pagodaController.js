6e34afb4b8afc751de5eecf0391f7615b4c0da07
HAKHEO <pigpannyaung@gmail.com>
2026-07-18 14:11:08 +0630

    Pagination

--- controllers/pagodaController.js
+++ controllers/pagodaController.js
@@ -141,7 +141,7 @@ export const pagodaCreate = asyncHandel(async (req, res) => {
 export const pagodaList = asyncHandel(async (req, res) => {
     try {
         const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 2;
+        const limit = Number(req.query.limit) || 10;
 
         const offset = (page - 1) * limit
 

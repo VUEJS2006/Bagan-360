c85016250e2ded88c3d15ba8b078bda50406675f
HAKHEO <pigpannyaung@gmail.com>
2026-07-21 17:17:56 +0630

    Res

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -105,3 +105,172 @@ export const restaurantList = asyncHandel(async (req, res) => {
 
     }
 })
+
+export const restaurantUpdate = asyncHandel(async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        let { name, location, address, dishes, phone, description } = req.body;
+
+        const [restaurant] = await db.query(`SELECT * FROM restaurants WHERE id = ?`, [id]);
+        if (restaurant.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Restaurant not found"
+            });
+        }
+        if (!dishes) {
+            dishes = [];
+        }
+
+        if (typeof dishes === "string") {
+            try {
+                dishes = JSON.parse(dishes);
+            } catch (error) {
+                dishes = dishes
+                    .split(",")
+                    .map(item => item.trim())
+                    .filter(Boolean);
+            }
+        }
+
+        let OldImage = restaurant[0].image;
+        if (req.file) {
+            if (restaurant[0].image) {
+                const oldPath = path.join(process.cwd(), restaurant[0].image)
+
+                if (fs.existsSync(oldPath)) {
+                    fs.unlinkSync(oldPath)
+                }
+            }
+
+            const uploadFolder = path.join(
+                process.cwd(),
+                "images",
+                "hotel"
+            );
+            if (!fs.existsSync(uploadFolder)) {
+                fs.mkdirSync(uploadFolder, { recursive: true })
+            }
+
+            const fileName = `${uuid()}.webp`;
+            const savePath = path.join(uploadFolder, fileName)
+            await sharp(req.file.buffer)
+                .resize({
+                    width: 1920,
+                    withoutEnlargement: true
+                })
+                .webp({
+                    quality: 90
+                })
+                .toFile(savePath);
+
+
+            OldImage = ` images/restaurant/${fileName}`;
+            let UpdateImage = OldImage;
+        }
+
+        const [data] = await db.query(
+            `
+            UPDATE  restaurants SET
+            name=?,
+            location=?,
+            address=?,
+            dishes=?,
+            phone=?,
+            description=?,
+            image=?
+            WHERE id= ?
+            `,
+            [
+                name, location, address, JSON.stringify(dishes), phone, description, UpdateImage, id
+            ]
+        );
+
+        return res.status(200).json({
+            message: "Restaurant Update Success",
+            success: true,
+            data
+        })
+
+    } catch (error) {
+
+        console.log(error);
+
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+})
+
+export const restaurantDelete = asyncHandel(async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const [restaurant] = await db.query("SELECT *  FROM restaurants WHERE id = ?", [id]);
+        if (restaurant.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Restaurant not found"
+            });
+        }
+        if (restaurant[0].image) {
+            const oldPath = path.join(process.cwd(), restaurant[0].image);
+            if (fs.existsSync(oldPath)) {
+                fs.unlinkSync(oldPath)
+            }
+        }
+        await db.query("DELETE FROM restaurants WHERE id = ?", [id]);
+        res.status(200).json({
+            success: true,
+            message: "Restaurants deleted successfully"
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+})
+
+export const restaurantDetails = asyncHandel(async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const [data] = await db.query(
+            `
+        SELECT 
+        id,
+        name,
+        location,
+        address,
+        phone,
+        description,
+        dishes,
+        image,
+        DATE_FORMAT(created_at, '%d-%m-%Y') as created_at
+        FROM restaurants
+        WHERE id = ?
+        `,
+            [id]
+        )
+        res.status(200).json({
+            success: true,
+            data
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+})
--- routers/restaurantRouter.js
+++ routers/restaurantRouter.js
@@ -1,4 +1,4 @@
-import { restaurantCreate, restaurantList } from "../controllers/restaurantController.js";
+import { restaurantCreate, restaurantList, restaurantUpdate, restaurantDelete, restaurantDetails } from "../controllers/restaurantController.js";
 import { upload } from "../middlewares/upload.js";
 import { authenticated, isAdmin } from "../middlewares/authenticatedMiddleware.js";
 import express from "express";
@@ -7,8 +7,9 @@ const router = express.Router()
 // Admin
 router.post("/admin/restaurant/create", authenticated, isAdmin, upload.single("image"), restaurantCreate);
 router.get('/admin/restaurant/list', authenticated, isAdmin, restaurantList)
-
-
+router.put('/admin/restaurant/update/:id', authenticated, isAdmin, restaurantUpdate)
+router.delete('/admin/restaurant/delete/:id', authenticated, isAdmin, restaurantDelete)
 // Mobile
 router.get('/mobile/restaurant/list', authenticated, restaurantList);
+router.get('/mobile/restaurant/details/:id', authenticated, restaurantDetails);
 export default router;

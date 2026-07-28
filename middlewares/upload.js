2a2ffc8d0462e642620bb87cd3ff2cd5d8fa752c
HAKHEO <pigpannyaung@gmail.com>
2026-07-28 22:45:33 +0630

    Imagee

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 export const upload = multer({
     storage,
     limits: {
-        fileSize: 10 * 1024 * 1024 // 10MB
+        fileSize: 20 * 1024 * 1024 // 10MB
     },
     fileFilter: (req, file, cb) => {
 

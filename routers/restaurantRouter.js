f1da186a7a314c79a1bcba7168c5ce032aec104a
HAKHEO <pigpannyaung@gmail.com>
2026-08-03 13:31:51 +0630

    Restaurants

--- routers/restaurantRouter.js
+++ routers/restaurantRouter.js
@@ -5,10 +5,10 @@ import express from "express";
 
 const router = express.Router()
 // Admin
-router.post("/admin/restaurant/create", authenticated, isAdmin, upload.single("image"), restaurantCreate);
-router.get('/admin/restaurant/list', authenticated, isAdmin, restaurantList)
-router.put('/admin/restaurant/update/:id', authenticated, isAdmin, upload.single("image"),restaurantUpdate)
-router.delete('/admin/restaurant/delete/:id', authenticated, isAdmin, restaurantDelete)
+router.post("/admin/restaurant/create", authenticated, upload.single("image"), restaurantCreate);
+router.get('/admin/restaurant/list', authenticated,restaurantList)
+router.put('/admin/restaurant/update/:id', authenticated, upload.single("image"),restaurantUpdate)
+router.delete('/admin/restaurant/delete/:id', authenticated, restaurantDelete)
 // Mobile
 router.get('/mobile/restaurant/list', authenticated, restaurantList);
 router.get('/mobile/restaurant/details/:id', authenticated, restaurantDetails);

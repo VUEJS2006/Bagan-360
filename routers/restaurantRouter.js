1390879b1281a5e29558dba2a4318c76f8c05588
HAKHEO <pigpannyaung@gmail.com>
2026-08-03 14:03:39 +0630

    Restaurants

--- routers/restaurantRouter.js
+++ routers/restaurantRouter.js
@@ -1,4 +1,4 @@
-import { restaurantCreate, restaurantList, restaurantUpdate, restaurantDelete, restaurantDetails } from "../controllers/restaurantController.js";
+import { restaurantCreate, restaurantMobileList,restaurantList, restaurantUpdate, restaurantDelete, restaurantDetails } from "../controllers/restaurantController.js";
 import { upload } from "../middlewares/upload.js";
 import { authenticated, isAdmin } from "../middlewares/authenticatedMiddleware.js";
 import express from "express";
@@ -10,6 +10,6 @@ router.get('/admin/restaurant/list', authenticated,restaurantList)
 router.put('/admin/restaurant/update/:id', authenticated, upload.single("image"),restaurantUpdate)
 router.delete('/admin/restaurant/delete/:id', authenticated, restaurantDelete)
 // Mobile
-router.get('/mobile/restaurant/list', authenticated, restaurantList);
+router.get('/mobile/restaurant/list', authenticated, restaurantMobileList);
 router.get('/mobile/restaurant/details/:id', authenticated, restaurantDetails);
 export default router;

0e9f9db887602fb31e119079bf2b66e114bec361
HAKHEO <pigpannyaung@gmail.com>
2026-09-04 15:04:23 +0630

    package

--- routers/packageRouter.js
+++ routers/packageRouter.js
@@ -5,7 +5,7 @@ import express from "express";
 
 const router = express.Router()
 // Admin
-router.post("/admin/package/create", authenticated, isAdmin, upload.array("images"), packageCreate);
+router.post("/admin/package/create", authenticated, isAdmin, upload.array("images",3), packageCreate);
 
 
 // Mobile

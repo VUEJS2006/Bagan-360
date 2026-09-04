cf1980958e2f104f3d971b722b79fc36c36f8baa
HAKHEO <pigpannyaung@gmail.com>
2026-09-04 16:23:56 +0630

    package all Fine

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -144,7 +144,7 @@ export const packageList = asyncHandel(async (req, res) => {
              GROUP BY p.id 
             `
         )
-        return req.status(200).json({
+        return res.status(200).json({
             message: "Package List Success",
             data: data.length,
             data

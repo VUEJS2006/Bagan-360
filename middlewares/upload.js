e0cc82361c2a0a41f21e83deaab196ec01da7fc0
HAKHEO <pigpannyaung@gmail.com>
2026-09-04 15:09:12 +0630

    package

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,12 +1,15 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.memoryStorage();
 
 export const upload = multer({
     storage,
+
     limits: {
-        fileSize: 20 * 1024 * 1024 // 10MB
+        fileSize: 20 * 1024 * 1024 // 20MB
     },
+
     fileFilter: (req, file, cb) => {
 
         const allowTypes = [
@@ -17,11 +20,23 @@ export const upload = multer({
             "image/avif",
         ];
 
-        if (allowTypes.includes(file.mimetype)) {
+        const ext = path.extname(file.originalname).toLowerCase();
+
+        const allowExtensions = [
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".webp",
+            ".avif"
+        ];
+
+        if (
+            allowTypes.includes(file.mimetype) ||
+            allowExtensions.includes(ext)
+        ) {
             cb(null, true);
         } else {
             cb(new Error("Only image files are allowed."));
         }
-
     }
 });

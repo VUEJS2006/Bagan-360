91a3d77de1c8e811b56223449dc5e3df978ed7ea
HAKHEO <pigpannyaung@gmail.com>
2026-09-10 16:14:32 +0630

    Th

--- controllers/thonebaneBookingController.js
+++ controllers/thonebaneBookingController.js
@@ -396,24 +396,21 @@ export const thonebaneMobileBooking = asyncHandel(async (req, res) => {
         b.id AS booking_id,
         b.user_id,
         b.shop_id,
-        b.thonebane_id,
-        
+        b.thonebane_id
         b.customer_name,
-        b.customer_phone,
-        DATE_FORMAT(b.booking_date, '%d-%m-%Y') AS booking_date,
+        b.customer_phone
+        DATE_FORMAT(
+            b.booking_date,
+            '%d-%m-%Y'
+        ) AS booking_date
         b.passenger_count,
         b.status,
         b.note,
-        b.price,
-
+        b.price
         t.name AS thonebane_name,
-        t.phone,
         t.location,
-        t.status,
-        t.image,
-
+        t.image
         s.shop_name
-
         FROM thonebane_bookings b JOIN thonebanes t ON b.thonebane_id = t.id
         JOIN shops s ON b.shop_id = s.id 
 

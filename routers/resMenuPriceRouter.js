7a2b928d08eab698a80c51967d0711fe075b0641
HAKHEO <pigpannyaung@gmail.com>
2026-09-13 14:07:45 +0630

    Res Menu Price

--- routers/resMenuPriceRouter.js
+++ routers/resMenuPriceRouter.js
@@ -6,7 +6,7 @@ import express from "express";
 const router = express.Router()
 // Admin
 router.get('/admin/res/menu/price/list', authenticated, resMenuPriceList)
-router.post('/admin//res/menu/price/create', authenticated, restMenuPriceCreate);
+router.post('/admin/res/menu/price/create', authenticated, restMenuPriceCreate);
 router.put('/admin/res/menu/price/update/:id', authenticated, resMenuPriceUpdate)
 router.delete('/admin/res/menu/price/delete/:id', authenticated, resMenuPriceDelete)
 
